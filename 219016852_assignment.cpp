0d9f2799cf713ef1092d141edf7f99d18498ffbb
GokuMaxwell <61115214+GokuMaxwell@users.noreply.github.com>
2021-07-15 21:33:54 +0000

    Add files via upload

---
+++ 219016852_assignment.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+ int main()
+{
+    using namespace std;
+    int x;
+    int y;
+
+    cout<<"enter a number"<<endl;
+    cin>>x;
+    cout<<"square root of a is"<<y;
+    
+    b=sqrt(x);
+    cout<<"square root of a is "<<y;
+}
